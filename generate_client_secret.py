5b374df1dedcdf7f4cd0e687bec5fe32471ca40d
Faruq <faruqabdulsalam@yahoo.com>
2023-04-04 17:51:45 +0100

    Refactor secret key generator function

--- generate_client_secret.py
+++ generate_client_secret.py
@@ -5,16 +5,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def generate_token():
+def generate_secret_key():
 
         with open(os.environ.get('filename'), "r") as f:
             private_key = f.read()
+
         team_id = os.environ.get('team_id')
         client_id = os.environ.get('client_id')
         key_id = os.environ.get('key_id')
-        validity_minutes = 180
+
         timestamp_now = datetime.datetime.now().timestamp()
-        timestamp_exp = timestamp_now + (24 * 60 * 60 * validity_minutes) # 6 months
+        timestamp_exp = timestamp_now + (86400 * 180) # 6 months
+
+        headers={"kid": key_id}
         data = {
                 "iss": team_id,
                 "iat": timestamp_now,
@@ -22,7 +25,9 @@ def generate_token():
                 "aud": "https://appleid.apple.com",
                 "sub": client_id
             }
-        token = jwt.encode(payload=data, key=private_key, algorithm="ES256", headers={"kid": key_id})
-        print('token', token)
 
-token = generate_token()
+        secret_key = jwt.encode(payload=data, key=private_key, algorithm="ES256", headers=headers)
+        return secret_key
+
+client_secret_key = generate_secret_key()
+print('client_secret_key', client_secret_key)
